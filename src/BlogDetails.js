0ffb5dad84ca24715003aca18cb1738d4a630560
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-03-23 16:46:02 +0800

    Add functional delete button in each blog in the BlogDetails.js

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,20 @@
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import useFetch from "./useFetch";
+import { useHistory } from "react-router-dom/cjs/react-router-dom";
 
 const BlogDetails = () => {
   const { id } = useParams()
   const {data: blog, error, isPending} = useFetch("http://localhost:8000/blogs/" + id)
-  
+  const pageHistory = useHistory()
+
+  const handleDelete = () => {
+    fetch("http://localhost:8000/blogs/" + id, {
+      method: "DELETE"
+    }).then(() => {
+      pageHistory.push("/")
+    })
+  }
+
   return ( 
     <div className="blog-details">
       { isPending && <div>Loading...</div> }
@@ -16,6 +26,7 @@ const BlogDetails = () => {
           <div>{ blog.body }</div>
         </article>
       )}
+      <button onClick={handleDelete}>Delete</button>
     </div>
    );
 }
